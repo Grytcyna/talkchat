594352e7fa3b828c1f8e7b47c3bf7e4246e0003b
Grytcyna <grytcyna@gmail.com>
2024-11-14 17:06:27 +0200

    first init

---
+++ src/main/groovy/com/grytsyna/talkchat/TalkchatApplication.groovy
@@ -0,0 +1,13 @@
+package com.grytsyna.talkchat
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class TalkchatApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(TalkchatApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/grytsyna/talkchat/TalkchatApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.grytsyna.talkchat
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TalkchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
